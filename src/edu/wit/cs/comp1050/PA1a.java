65bca298f0e7cf9363abaf10289e6f7a42e3f130
Edgar Romero <fuentese1@wit.edu>
2017-01-09 01:20:31 -0500

    Hello World!

--- src/edu/wit/cs/comp1050/PA1a.java
+++ src/edu/wit/cs/comp1050/PA1a.java
@@ -5,7 +5,7 @@ public class PA1a {
 
 	// This will print out Hello, World in the terminal
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hello World!");
 	}
 
 }
