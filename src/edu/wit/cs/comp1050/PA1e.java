20970cc39768b76fa319fd2fd38b8e56c0ef5be2
Edgar Romero <fuentese1@wit.edu>
2017-01-13 17:11:54 -0500

    Input string, outputs all capitals found in string

--- src/edu/wit/cs/comp1050/PA1e.java
+++ src/edu/wit/cs/comp1050/PA1e.java
@@ -1,22 +1,67 @@
 package edu.wit.cs.comp1050;
 
-//TODO: document this class
+import java.util.Scanner;
+
+/**
+ * Solution for the fifth Part
+ * 
+ * @author Edgar Romero
+ *
+ */
 public class PA1e {
-	
+
 	/**
-	 * Counts the number of upper case characters
-	 * within the supplied string
+	 * Counts the number of upper case characters within the supplied string
 	 * 
-	 * @param s input string
+	 * @param s
+	 *            input string
 	 * @return number of upper case characters
 	 */
 	public static int numUpperCase(String s) {
-		return 0; // TODO: replace with your method implementation
+		int upperCaseCount = 0;
+		// a for loop is used so the string condition is tested before executing
+		for (int i = 0; i < s.length(); i++) { 
+			for (char c = 'A'; c <= 'Z'; c++) {
+				if (s.charAt(i) == c) { 
+					upperCaseCount++; //counts uppercase 
+				}
+			}
+		}
+		return upperCaseCount;
 	}
 
-	// TODO: document this method
+	/**
+	 * Starts the program, user inputs string, outputs the number of uppercase
+	 * letters
+	 * 
+	 * @param args
+	 *            command-line arguments ignored
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+		Scanner sc = new Scanner(System.in);
 
+		String input;
+
+		System.out.print("Enter a string: ");
+		input = sc.nextLine();
+		// if no string is entered 
+		if (input.equals(null) || input.equals("")){
+			System.out.printf("There are no uppercase characters.%n");
+			System.exit(0);
+		}
+		// if string has 0 uppercases
+		else if (numUpperCase(input) == 0){
+			System.out.printf("There are no uppercase characters.%n");
+			System.exit(0);
+		}
+		// if string has exactly 1 uppercase
+		else if (numUpperCase(input) == 1){
+			System.out.printf("There is %d uppercase character in the string.%n", numUpperCase(input));
+		}
+
+		else {
+			System.out.printf("There are %d uppercase characters in the string.%n", numUpperCase(input));
+		}
+	}
 }
+
